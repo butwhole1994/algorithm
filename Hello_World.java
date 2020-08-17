84593613fbebda849b828557e29a29f632d900a1
butwhole1994 <61606289+butwhole1994@users.noreply.github.com>
2020-08-17 22:59:27 +0900

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,4 +1,4 @@
-//https://www.acmicpc.net/problem/2557 น้มุ
+//https://www.acmicpc.net/problem/2557
 public class Hello_World {
 	public static void main(String[] agrs) {
 		System.out.println("Hello World!");
