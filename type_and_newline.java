2cd9155ef4c835a8902679c5729142b269f56515
butwhole1994 <61606289+butwhole1994@users.noreply.github.com>
2020-08-17 23:07:07 +0900

    Add files via upload

---
+++ type_and_newline.java
@@ -0,0 +1,18 @@
+
+public class type_and_newline {
+	public static void main (String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		//System.out.println()은 쓰고 나서 줄을 바꾼다.
+		//System.out.print()는 줄바꿈이 없다.
+	}
+}
+
+// 또는
+//public class type_and_newline {
+//	public static void main (String[] args) {
+//		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+//	}
+//}
+//이런 식으로 System.out.println("강한친구 대한육군\n강한친구 대한육군");
+//안에서 \n을 동한 줄바꿈을 이용해 바꿀 수도 있다.
